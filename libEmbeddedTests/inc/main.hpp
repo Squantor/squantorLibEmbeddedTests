05a6ef417f5a6e502a9ae99e819752aa2de3a26c
Squantor <squantmuts@gmail.com>
2020-08-14 11:12:41 +0200

    configured doxygen

---
+++ libEmbeddedTests/inc/main.hpp
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2020-2020 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/**
+ *  Include definitions for main.cpp if any
+ */
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#endif
