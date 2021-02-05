da8ab06e5369a123f3c9eb7e56e5936fc93b96cc
Squantor <squantmuts@gmail.com>
2021-02-05 11:55:34 +0100

    WIP tests for moving average reset

--- libEmbeddedTests/src/test_moving_average.c
+++ libEmbeddedTests/src/test_moving_average.c
@@ -14,7 +14,28 @@
 #include <stdint.h>
 #include <moving_average.h>
 
-MINUNIT_ADD(movingAverageUint16)
+#define TESTU16_TYPE    uint16_t
+#define TESTU16_SIZE    4
+
+MOVING_AVERAGE_VARS(testU16, TESTU16_TYPE, TESTU16_SIZE);
+MOVING_AVERAGE_PROTO(testU16, TESTU16_TYPE);
+MOVING_AVERAGE_FUNCTIONS(testU16, TESTU16_TYPE, TESTU16_SIZE);
+
+void movingAverageTestSetup(void)
 {
-    
+    testU16Reset();
+}
+
+void movingAverageTestTeardown(void)
+{
+
+}
+
+MINUNIT_ADD(movingAverageUint16Reset)
+{
+    minUnitCheck(testU16Get() == 0);
+    testU16Add(10);
+    minUnitCheck(testU16Get() != 0);
+    testU16Reset();
+    minUnitCheck(testU16Get() == 0);
 }
