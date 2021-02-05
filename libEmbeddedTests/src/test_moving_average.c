314a7378094d18104bd3eeee220c71e14f9ef827
Squantor <squantmuts@gmail.com>
2021-02-05 12:55:25 +0100

    added moving average tests for uint32 and doubles

--- libEmbeddedTests/src/test_moving_average.c
+++ libEmbeddedTests/src/test_moving_average.c
@@ -21,9 +21,25 @@ MOVING_AVERAGE_VARS(testU16, TESTU16_TYPE, TESTU16_SIZE);
 MOVING_AVERAGE_PROTO(testU16, TESTU16_TYPE);
 MOVING_AVERAGE_FUNCTIONS(testU16, TESTU16_TYPE, TESTU16_SIZE);
 
+#define TESTU32_TYPE    uint32_t
+#define TESTU32_SIZE    3
+
+MOVING_AVERAGE_VARS(testU32, TESTU32_TYPE, TESTU32_SIZE);
+MOVING_AVERAGE_PROTO(testU32, TESTU32_TYPE);
+MOVING_AVERAGE_FUNCTIONS(testU32, TESTU32_TYPE, TESTU32_SIZE);
+
+#define TESTDOUBLE_TYPE    double
+#define TESTDOUBLE_SIZE    5
+
+MOVING_AVERAGE_VARS(testDouble, TESTDOUBLE_TYPE, TESTDOUBLE_SIZE);
+MOVING_AVERAGE_PROTO(testDouble, TESTDOUBLE_TYPE);
+MOVING_AVERAGE_FUNCTIONS(testDouble, TESTDOUBLE_TYPE, TESTDOUBLE_SIZE);
+
 void movingAverageTestSetup(void)
 {
     testU16Reset();
+    testU32Reset();
+    testDoubleReset();
 }
 
 void movingAverageTestTeardown(void)
@@ -33,15 +49,18 @@ void movingAverageTestTeardown(void)
 
 MINUNIT_ADD(movingAverageUint16Reset)
 {
+    movingAverageTestSetup();
     minUnitCheck(testU16Get() == 0);
     testU16Add(10);
     minUnitCheck(testU16Get() != 0);
     testU16Reset();
     minUnitCheck(testU16Get() == 0);
+    movingAverageTestTeardown();
 }
 
 MINUNIT_ADD(movingAverageUint16Get)
 {
+    movingAverageTestSetup();
     minUnitCheck(testU16Get() == 0);
     testU16Add(1);
     testU16Add(2);
@@ -52,4 +71,37 @@ MINUNIT_ADD(movingAverageUint16Get)
     minUnitCheck(testU16Get() == 3);
     testU16Add(6);
     minUnitCheck(testU16Get() == 4);
+    movingAverageTestTeardown();
+}
+
+MINUNIT_ADD(movingAverageUint32Get)
+{
+    movingAverageTestSetup();
+    minUnitCheck(testU32Get() == 0);
+    testU32Add(1);
+    testU32Add(2);
+    testU32Add(3);
+    testU32Add(4);
+    minUnitCheck(testU32Get() == 3);
+    testU32Add(5);
+    minUnitCheck(testU32Get() == 4);
+    testU32Add(6);
+    minUnitCheck(testU32Get() == 5);
+    movingAverageTestTeardown();
+}
+
+MINUNIT_ADD(movingAverageDoubleGet)
+{
+    movingAverageTestSetup();
+    minUnitCheck(testDoubleGet() == 0);
+    testDoubleAdd(1.1);
+    testDoubleAdd(2.2);
+    testDoubleAdd(3.3);
+    testDoubleAdd(4.4);
+    minUnitCheck(testDoubleGet() == 2.2);
+    testDoubleAdd(5);
+    minUnitCheck(testDoubleGet() == 3.2);
+    testDoubleAdd(6);
+    minUnitCheck(testDoubleGet() == 4.18);
+    movingAverageTestTeardown();
 }
