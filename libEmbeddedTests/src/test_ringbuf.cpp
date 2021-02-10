e4de10ac37fad04411f784dd82fcd26669de0ce0
Squantor <squantmuts@gmail.com>
2021-02-10 22:06:38 +0100

    added all tests

--- libEmbeddedTests/src/test_ringbuf.cpp
+++ libEmbeddedTests/src/test_ringbuf.cpp
@@ -21,3 +21,110 @@ MINUNIT_ADD(RingBufferCppEmpty)
     minUnitCheck(RingBufferDutU16.empty() == true);
     minUnitCheck(RingBufferDutU16.full() == false);
 }
+
+MINUNIT_ADD(RingBufferCppPushBack)
+{
+    uint16_t number = 42;
+    RingBufferDutU16.reset();
+    minUnitCheck(RingBufferDutU16.empty() == true);
+    minUnitCheck(RingBufferDutU16.PushBack(number) == true);
+    minUnitCheck(RingBufferDutU16.empty() == false);
+}
+
+MINUNIT_ADD(RingBufferCppPushBackFull)
+{
+    uint16_t number = 42;
+    RingBufferDutU16.reset();
+    minUnitCheck(RingBufferDutU16.empty() == true);
+    for(uint16_t i = 0; i < 10; i++)
+    {
+        minUnitCheck(RingBufferDutU16.PushBack(i) == true);
+    }
+    minUnitCheck(RingBufferDutU16.PushBack(number) == false);
+    minUnitCheck(RingBufferDutU16.full() == true);
+}
+
+MINUNIT_ADD(RingBufferCppPushFront)
+{
+    uint16_t number = 42;
+    RingBufferDutU16.reset();
+    minUnitCheck(RingBufferDutU16.empty() == true);
+    minUnitCheck(RingBufferDutU16.PushFront(number) == true);
+    minUnitCheck(RingBufferDutU16.empty() == false);
+}
+
+MINUNIT_ADD(RingBufferCppPushFrontFull)
+{
+    uint16_t number = 42;
+    RingBufferDutU16.reset();
+    minUnitCheck(RingBufferDutU16.empty() == true);
+    for(uint16_t i = 0; i < 10; i++)
+    {
+        minUnitCheck(RingBufferDutU16.PushFront(i) == true);
+    }
+    minUnitCheck(RingBufferDutU16.PushFront(number) == false);
+    minUnitCheck(RingBufferDutU16.full() == true);
+}
+
+MINUNIT_ADD(RingBufferCppPopBack)
+{
+    uint16_t number = 42;
+    uint16_t output = 0;
+    RingBufferDutU16.reset();
+    minUnitCheck(RingBufferDutU16.PopBack(output) == false);
+    minUnitCheck(RingBufferDutU16.PushFront(number) == true);
+    minUnitCheck(RingBufferDutU16.PopBack(output) == true);
+    minUnitCheck(output == 42);
+    minUnitCheck(RingBufferDutU16.PopBack(output) == false);
+    for(uint16_t i = 0; i < 10; i++)
+    {
+        RingBufferDutU16.PushFront(i);
+    }
+    for(uint16_t i = 0; i < 10; i++)
+    {
+        minUnitCheck(RingBufferDutU16.PopBack(output) == true);
+        minUnitCheck(output == i);
+    }
+    minUnitCheck(RingBufferDutU16.PopBack(output) == false);
+    for(uint16_t i = 0; i < 10; i++)
+    {
+        RingBufferDutU16.PushBack(i);
+    }
+    for(uint16_t i = 0; i < 10; i++)
+    {
+        minUnitCheck(RingBufferDutU16.PopBack(output) == true);
+        minUnitCheck(output == 9 - i);
+    }
+    minUnitCheck(RingBufferDutU16.PopBack(output) == false);
+}
+
+MINUNIT_ADD(RingBufferCppPopFront)
+{
+    uint16_t number = 42;
+    uint16_t output = 0;
+    minUnitCheck(RingBufferDutU16.PopFront(output) == false);
+    minUnitCheck(RingBufferDutU16.PushFront(number) == true);
+    minUnitCheck(RingBufferDutU16.PopFront(output) == true);
+    minUnitCheck(output == 42);
+    minUnitCheck(RingBufferDutU16.PopFront(output) == false);
+    for(uint16_t i = 0; i < 10; i++)
+    {
+        RingBufferDutU16.PushFront(i);
+    }
+    for(int i = 9; i >= 0; i--)
+    {
+        minUnitCheck(RingBufferDutU16.PopFront(output) == true);
+        minUnitCheck(output == i);
+    }
+    minUnitCheck(RingBufferDutU16.PopFront(output) == false);
+    for(uint16_t i = 0; i < 10; i++)
+    {
+        RingBufferDutU16.PushBack(i);
+    }
+    for(uint16_t i = 0; i < 10; i++)
+    {
+        minUnitCheck(RingBufferDutU16.PopFront(output) == true);
+        minUnitCheck(output == i);
+    }
+    minUnitCheck(RingBufferDutU16.PopFront(output) == false);
+}
