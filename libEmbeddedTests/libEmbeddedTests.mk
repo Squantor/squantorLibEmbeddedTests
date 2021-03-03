2b77266b03762874bfba1b38e21cd3cdd4ddc8e1
Squantor <squantmuts@gmail.com>
2021-03-03 22:43:49 +0100

    initial start of testing atomics

--- libEmbeddedTests/libEmbeddedTests.mk
+++ libEmbeddedTests/libEmbeddedTests.mk
@@ -31,7 +31,8 @@ $(PROJECT)/src/test_prompt_mini.c \
 $(PROJECT)/src/test_ringbuf_macro.c \
 $(PROJECT)/src/test_moving_average.c \
 $(PROJECT)/src/test_moving_average.cpp \
-$(PROJECT)/src/test_ringbuf.cpp
+$(PROJECT)/src/test_ringbuf.cpp \
+$(PROJECT)/src/test_atomic.cpp
 
 INCLUDES += -I$(PROJECT)/inc
 
---
+++ libEmbeddedTests/src/test_atomic.cpp
@@ -0,0 +1,24 @@
+/*
+SPDX-License-Identifier: MIT
+
+Copyright (c) 2021 Bart Bilos
+For conditions of distribution and use, see LICENSE file
+ */
+
+/** \file testArray.cpp
+
+\brief Tests for util::atomic
+ 
+Tests for util::atomic
+*/
+#include <MinUnit.h>
+
+
+/** \brief util::atomic test size()
+
+testing the util::atomic method
+*/
+MINUNIT_ADD(testAtomic)
+{
+
+}
