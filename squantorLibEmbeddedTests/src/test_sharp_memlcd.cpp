3f37c2b4ac43d169bfcfad306b41257a845ca18f
Squantor <squantmuts@gmail.com>
2021-09-11 16:52:47 +0200

    added putpixel tests

--- squantorLibEmbeddedTests/src/test_sharp_memlcd.cpp
+++ squantorLibEmbeddedTests/src/test_sharp_memlcd.cpp
@@ -13,6 +13,7 @@ util::sharpMemLcd<lcdTestConfig> testDevice;
 
 static void testSharpMemLcdSetup(minunitState *testResults) 
 {
+    testDevice.init();
     testResults = testResults; /*!< supress warning */
 }
 
@@ -23,9 +24,22 @@ static void testSharpMemLcdTeardown(minunitState *testResults)
 
 MINUNIT_ADD(testSharpMemLcdInit, testSharpMemLcdSetup, testSharpMemLcdTeardown) 
 {
-    testDevice.init();
     minUnitCheck(testDevice.frameBuffer[0] == 0x8001);
     minUnitCheck(testDevice.frameBuffer[3] == 0x0000);
     minUnitCheck(testDevice.frameBuffer[8] == 0x8003);
     minUnitCheck(testDevice.frameBuffer[124] == 0x8020);
+}
+
+MINUNIT_ADD(testSharpMemLcdPutPixel, testSharpMemLcdSetup, testSharpMemLcdTeardown) 
+{
+    testDevice.putPixel(0,0,1);
+    testDevice.putPixel(1,1,1);
+    testDevice.putPixel(18,1,1);
+    testDevice.putPixel(16,1,1);
+    minUnitCheck(testDevice.frameBuffer[1] == 0x0001);
+    minUnitCheck(testDevice.frameBuffer[5] == 0x0002);
+    minUnitCheck(testDevice.frameBuffer[6] == 0x0005);
+    // test clearing
+    testDevice.putPixel(18,1,0);
+    minUnitCheck(testDevice.frameBuffer[6] == 0x0001);
 }
