8475f8ca478cca52e37148ef1b1208c1c0a13cc2
Squantor <squantmuts@gmail.com>
2022-01-19 18:52:42 +0100

    added additional test for indexing function

--- squantorLibEmbeddedTests/src/test_font8x8.c
+++ squantorLibEmbeddedTests/src/test_font8x8.c
@@ -8,7 +8,12 @@
 #include <font_8x8.h>
 
 MINUNIT_ADD(testFont8x8, NULL, NULL) {
+  const uint8_t* begin = &font8x8[0];
+  const uint8_t* end = &font8x8[sizeof(font8x8)];
   for (int i = 0; i < 128; i++) {
-    minUnitCheck(ascii2font8x8[i] < sizeof(font8x8));
+    minUnitCheck(ascii2font8x8Index[i] < sizeof(font8x8));
+    const uint8_t* bitmapPtr = ascii2Font8x8(i);
+    minUnitCheck(bitmapPtr >= begin);
+    minUnitCheck(bitmapPtr < end);
   }
 }
