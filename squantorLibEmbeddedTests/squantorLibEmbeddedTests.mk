de29bd42c74dc0ad8e9ab9c13fef5076938e47d5
Squantor <squantmuts@gmail.com>
2022-03-11 16:13:53 +0100

    started with test

--- squantorLibEmbeddedTests/squantorLibEmbeddedTests.mk
+++ squantorLibEmbeddedTests/squantorLibEmbeddedTests.mk
@@ -35,7 +35,8 @@ $(PROJECT)/src/test_ringbuf.cpp \
 $(PROJECT)/src/test_atomic.cpp \
 $(PROJECT)/src/test_sharp_memlcd.cpp \
 $(PROJECT)/src/test_font8x8.c \
-$(PROJECT)/src/test_bitzoom.c
+$(PROJECT)/src/test_bitzoom.c \
+$(PROJECT)/src/test_pulse_density.c
 
 INCLUDES += -I$(PROJECT)/inc
 
---
+++ squantorLibEmbeddedTests/src/test_pulse_density.c
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2022 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+#include <MinUnit.h>
+#include <pulse_density.h>
