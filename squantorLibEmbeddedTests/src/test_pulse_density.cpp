d43f6fbe7cc5d5f827e708b8b8f4a1c237952923
Squantor <squantmuts@gmail.com>
2022-03-13 14:02:07 +0100

    fixed tests C++ pulse density modulator

--- squantorLibEmbeddedTests/src/test_pulse_density.cpp
+++ squantorLibEmbeddedTests/src/test_pulse_density.cpp
@@ -6,3 +6,18 @@
  */
 #include <MinUnit.h>
 #include <pulse_density.hpp>
+
+MINUNIT_ADD(testPulseDensityModulator, NULL, NULL) {
+  util::PulseDensityModulator DUT(1000, 99);
+  minUnitCheck(DUT.sum == 0);
+  minUnitCheck(DUT.step == 99);
+  minUnitCheck(DUT.threshold == 1000);
+  minUnitCheck(DUT.output() == false);
+  minUnitCheck(DUT.sum == 99);
+  for (int i = 0; i < 9; i++) {
+    minUnitCheck(DUT.output() == false);
+  }
+  minUnitCheck(DUT.sum == 990);
+  minUnitCheck(DUT.output() == true);
+  minUnitCheck(DUT.sum == 89);
+}
