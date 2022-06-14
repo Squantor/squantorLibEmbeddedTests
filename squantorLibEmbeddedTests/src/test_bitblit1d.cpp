cfd90cef6004ec23b0ec05b038106dd60a0dd343
Squantor <squantmuts@gmail.com>
2022-06-14 22:47:34 +0200

    WIP bit blit 1d

--- squantorLibEmbeddedTests/src/test_bitblit1d.cpp
+++ squantorLibEmbeddedTests/src/test_bitblit1d.cpp
@@ -11,6 +11,13 @@
  */
 
 #include <MinUnit.h>
+#include <string.h>
 #include <stdint.h>
+#include <bitblit.hpp>
 
-MINUNIT_ADD(testBitBlit1D, NULL, NULL) {}
+MINUNIT_ADD(testBitBlit1D, NULL, NULL) {
+  uint8_t testDest[3]{0x00, 0x00, 0x00};
+  uint8_t testSrc[1]{0xFF};
+  util::bitblit1d(testDest, sizeof(testDest), 0, testSrc, 8);
+  minUnitCheck(testDest[0] == 0xFF);
+}
