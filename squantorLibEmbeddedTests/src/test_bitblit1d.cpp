c85a4fb701c359f7210dfe0e0cedd47bec206be0
Squantor <squantmuts@gmail.com>
2022-06-17 11:37:44 +0200

    WIP: test cases for bitBlit1D alignment cases

--- squantorLibEmbeddedTests/src/test_bitblit1d.cpp
+++ squantorLibEmbeddedTests/src/test_bitblit1d.cpp
@@ -15,9 +15,38 @@
 #include <stdint.h>
 #include <bitblit.hpp>
 
-MINUNIT_ADD(testBitBlit1D, NULL, NULL) {
-  uint8_t testDest[3]{0x00, 0x00, 0x00};
-  uint8_t testSrc[1]{0xFF};
-  util::bitblit1d(testDest, sizeof(testDest), 0, testSrc, 8);
-  minUnitCheck(testDest[0] == 0xFF);
+MINUNIT_ADD(testBitBlit1DCases, NULL, NULL) {
+  // we add one extra byte as canary to check out of bound writes
+  uint8_t testDest[5]{0x00, 0x00, 0x00, 0x00, 0x00};
+  uint8_t testSrc[3]{0xFF, 0xFF, 0xFF};
+  size_t testDestSize = sizeof(testDest) - 1;  // subtract one for canary
+  // single byte aligned
+  memset(testDest, 0, sizeof(testDest));
+  util::bitblit1d(testDest, testDestSize, 8, testSrc, 8);
+  minUnitCheck(testDest[1] == 0xFF);
+  // single byte crossing boundary
+  memset(testDest, 0, sizeof(testDest));
+  util::bitblit1d(testDest, testDestSize, 3, testSrc, 8);
+  minUnitCheck(testDest[0] == 0xF8);
+  minUnitCheck(testDest[1] == 0x07);
+  // single byte crossing boundary at the edge
+  memset(testDest, 0, sizeof(testDest));
+  util::bitblit1d(testDest, testDestSize, 28, testSrc, 8);
+  minUnitCheck(testDest[3] == 0xF0);
+  minUnitCheck(testDest[4] == 0x00);
+  // two byte aligned
+  memset(testDest, 0, sizeof(testDest));
+  util::bitblit1d(testDest, testDestSize, 8, testSrc, 16);
+  minUnitCheck(testDest[1] == 0xFF);
+  minUnitCheck(testDest[2] == 0xFF);
+  // two byte crossing boundary
+  // n byte aligned
+  // n byte aligned but with dangling few bits (special case)
+  // n byte crossing boundary
+  // less then one byte aligned
+  // less then one byte crossing
+}
+
+MINUNIT_ADD(testBitBlit1DOperations, NULL, NULL) {
+  minUnitPass();
 }
