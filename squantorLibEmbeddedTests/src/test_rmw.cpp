987aae95b325d86e44700dd3c3340c49e7ed0112
Squantor <squantmuts@gmail.com>
2022-06-19 18:36:07 +0200

    completed read modify write tests

--- squantorLibEmbeddedTests/src/test_rmw.cpp
+++ squantorLibEmbeddedTests/src/test_rmw.cpp
@@ -15,4 +15,48 @@
 #include <stdint.h>
 #include <bitblit.hpp>
 
-MINUNIT_ADD(testReadModifyWrite, NULL, NULL) {}
+MINUNIT_ADD(testReadModifyWrite, NULL, NULL) {
+  uint8_t testSource = 0x00;
+  uint8_t testDestination = 0x00;
+  // test AND
+  testSource = 0x0C;
+  testDestination = 0x3F;
+  util::readModifyWrite(&testDestination, &testSource, 0x3C, 2, util::bitblitOperation::OP_AND);
+  minUnitCheck(testDestination == 0x33);
+  // test MOV
+  testSource = 0xF;
+  testDestination = 0x00;
+  util::readModifyWrite(&testDestination, &testSource, 0x3C, 2, util::bitblitOperation::OP_NONE);
+  minUnitCheck(testDestination == 0x3C);
+  testSource = 0xF0;
+  testDestination = 0x00;
+  util::readModifyWrite(&testDestination, &testSource, 0x3C, -2, util::bitblitOperation::OP_NONE);
+  minUnitCheck(testDestination == 0x3C);
+  testSource = 0x0F;
+  testDestination = 0x01;
+  util::readModifyWrite(&testDestination, &testSource, 0x3C, 2, util::bitblitOperation::OP_NONE);
+  minUnitCheck(testDestination == 0x3D);
+  testSource = 0x0F;
+  testDestination = 0x91;
+  util::readModifyWrite(&testDestination, &testSource, 0x3C, 2, util::bitblitOperation::OP_NONE);
+  minUnitCheck(testDestination == 0xBD);
+  // test NOT
+  testSource = 0xF;
+  testDestination = 0xFF;
+  util::readModifyWrite(&testDestination, &testSource, 0x3C, 2, util::bitblitOperation::OP_NOT);
+  minUnitCheck(testDestination == 0xC3);
+  testSource = 0xF0;
+  testDestination = 0x0F;
+  util::readModifyWrite(&testDestination, &testSource, 0x3C, -2, util::bitblitOperation::OP_NOT);
+  minUnitCheck(testDestination == 0x03);
+  // test OR
+  testSource = 0x0C;
+  testDestination = 0x0F;
+  util::readModifyWrite(&testDestination, &testSource, 0x3C, 2, util::bitblitOperation::OP_OR);
+  minUnitCheck(testDestination == 0x3F);
+  // test XOR
+  testSource = 0x0F;
+  testDestination = 0x0F;
+  util::readModifyWrite(&testDestination, &testSource, 0x3C, 2, util::bitblitOperation::OP_XOR);
+  minUnitCheck(testDestination == 0x33);
+}
