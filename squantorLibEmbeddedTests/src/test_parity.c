cff2128e47334bc64fcbaa33a8b15e7aef9bb283
Bart Bilos <squantmuts+git@gmail.com>
2023-03-31 13:19:39 +0200

    Added parity calculation tests

--- squantorLibEmbeddedTests/src/test_parity.c
+++ squantorLibEmbeddedTests/src/test_parity.c
@@ -10,7 +10,35 @@
  * All tests for parity functions
  */
 #include <MinUnit.h>
+#include <parity.h>
 
 MINUNIT_ADD(testParityC, NULL, NULL) {
-  minUnitCheck(1 == 1);
+  // test cases from wikipedia
+  minUnitCheck(false == sq_parityEven((uint8_t)0b0000000u));
+  minUnitCheck(true == sq_parityEven((uint8_t)0b1010001u));
+  minUnitCheck(false == sq_parityEven((uint8_t)0b1101001u));
+  minUnitCheck(true == sq_parityEven((uint8_t)0b1111111u));
+
+  minUnitCheck(true == sq_parityOdd((uint8_t)0b0000000u));
+  minUnitCheck(false == sq_parityOdd((uint8_t)0b1010001u));
+  minUnitCheck(true == sq_parityOdd((uint8_t)0b1101001u));
+  minUnitCheck(false == sq_parityOdd((uint8_t)0b1111111u));
+
+  // my own test cases
+  minUnitCheck(false == sq_parityEven((uint8_t)0x03u));
+  minUnitCheck(false == sq_parityEven((uint16_t)0x1001u));
+  minUnitCheck(false == sq_parityEven((uint32_t)0x00303003u));
+  minUnitCheck(false == sq_parityEven((uint64_t)0x0060060030033060u));
+  minUnitCheck(true == sq_parityOdd((uint8_t)0x03u));
+  minUnitCheck(true == sq_parityOdd((uint16_t)0x1001u));
+  minUnitCheck(true == sq_parityOdd((uint32_t)0x00303003u));
+  minUnitCheck(true == sq_parityOdd((uint64_t)0x0060060030033060u));
+  minUnitCheck(true == sq_parityEven((uint8_t)0x10u));
+  minUnitCheck(true == sq_parityEven((uint16_t)0x1110u));
+  minUnitCheck(true == sq_parityEven((uint32_t)0x22220002u));
+  minUnitCheck(true == sq_parityEven((uint64_t)0x8221000200020001u));
+  minUnitCheck(false == sq_parityOdd((uint8_t)0x10u));
+  minUnitCheck(false == sq_parityOdd((uint16_t)0x1110u));
+  minUnitCheck(false == sq_parityOdd((uint32_t)0x22220002u));
+  minUnitCheck(false == sq_parityOdd((uint64_t)0x8221000200020001u));
 }
--- squantorLibEmbeddedTests/src/test_parity.cpp
+++ squantorLibEmbeddedTests/src/test_parity.cpp
@@ -10,7 +10,37 @@
  * All tests for parity functions
  */
 #include <MinUnit.h>
+#include <parity.hpp>
+
+using namespace util;
 
 MINUNIT_ADD(testParityCpp, NULL, NULL) {
-  minUnitCheck(1 == 1);
+  // test cases from wikipedia
+  minUnitCheck(false == parityEven(static_cast<uint8_t>(0b0000000u)));
+  minUnitCheck(true == parityEven(static_cast<uint8_t>(0b1010001u)));
+  minUnitCheck(false == parityEven(static_cast<uint8_t>(0b1101001u)));
+  minUnitCheck(true == parityEven(static_cast<uint8_t>(0b1111111u)));
+
+  minUnitCheck(true == parityOdd(0b0000000u));
+  minUnitCheck(false == parityOdd(0b1010001u));
+  minUnitCheck(true == parityOdd(0b1101001u));
+  minUnitCheck(false == parityOdd(0b1111111u));
+
+  // my own test cases
+  minUnitCheck(false == parityEven(0x03u));
+  minUnitCheck(false == parityEven(0x1001u));
+  minUnitCheck(false == parityEven(0x00303003u));
+  minUnitCheck(false == parityEven(0x0060060030033060u));
+  minUnitCheck(true == parityOdd(0x03u));
+  minUnitCheck(true == parityOdd(0x1001u));
+  minUnitCheck(true == parityOdd(0x00303003u));
+  minUnitCheck(true == parityOdd(0x0060060030033060u));
+  minUnitCheck(true == parityEven(0x10u));
+  minUnitCheck(true == parityEven(0x1110u));
+  minUnitCheck(true == parityEven(0x22220002u));
+  minUnitCheck(true == parityEven(0x8221000200020001u));
+  minUnitCheck(false == parityOdd(0x10u));
+  minUnitCheck(false == parityOdd(0x1110u));
+  minUnitCheck(false == parityOdd(0x22220002u));
+  minUnitCheck(false == parityOdd(0x8221000200020001u));
 }
