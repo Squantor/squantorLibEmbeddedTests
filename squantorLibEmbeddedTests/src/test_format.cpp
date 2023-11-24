65259996eb9287882a348bddb74030e349e55716
Bart Bilos <squantmuts+git@gmail.com>
2023-11-24 12:05:01 +0100

    added formatAppendDigit tests

--- squantorLibEmbeddedTests/src/test_format.cpp
+++ squantorLibEmbeddedTests/src/test_format.cpp
@@ -19,5 +19,17 @@ MINUNIT_ADD(testFormatAppendString, nullptr, nullptr) {
   std::span<char> temp = util::appendString(input, first);
   temp = util::appendString(temp, second);
   temp = util::appendString(temp, third);
-  minUnitCheck(strncmp(input.data(), output.data(), strlen(first.data())) == 0);
+  minUnitCheck(strncmp(input.data(), output.data(), strlen(output.data())) == 0);
+}
+
+MINUNIT_ADD(testFormatAppendDigit, nullptr, nullptr) {
+  std::array<char, 5> input;
+  std::array<char, 5> output{"12AC"};
+  std::span<char> temp;
+  temp = util::appendDigit(input, 1u);
+  temp = util::appendDigit(temp, 2u);
+  temp = util::appendDigit(temp, 0xA);
+  temp = util::appendDigit(temp, 0xC);
+  temp = util::appendDigit(temp, 0xF);
+  minUnitCheck(strncmp(input.data(), output.data(), strlen(output.data())) == 0);
 }
