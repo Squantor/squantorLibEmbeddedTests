01a83769e73923961d8e38f18b611ce5e1200e1e
Bart Bilos <squantmuts+git@gmail.com>
2023-11-24 11:41:42 +0100

    WIP testAppendString

--- squantorLibEmbeddedTests/src/test_format.cpp
+++ squantorLibEmbeddedTests/src/test_format.cpp
@@ -6,3 +6,15 @@
  */
 
 #include <format.hpp>
+#include <MinUnit.h>
+#include <cstring>
+#include <array>
+
+MINUNIT_ADD(testFormatAppendString, nullptr, nullptr) {
+  std::array<char, 20> input;
+  std::array<char, 20> output{"firstsecond"};
+  std::array<char, 8> first{"first"};
+  std::array<char, 8> second{"second"};
+
+  minUnitCheck(strncmp(input.data(), output.data(), strlen(first.data())) == 0);
+}
